9c5f68f91e593ba93e48699e4dfb84966ac42758
Diego Rios Vasquez <diegovasquezcolombia@gmail.com>
2021-09-07 19:30:23 -0500

    Add project files.

---
+++ EjerciciosHerramientasI/Ejercicios/EjercicioParcial.cs
@@ -0,0 +1,37 @@
+﻿using EjerciciosHerramientasI.Models;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace EjerciciosHerramientasI.Ejercicios
+{
+    public class EjercicioParcial
+    {
+        Pin pin;
+        List<int> password;
+        int[] genPass;
+        public EjercicioParcial()
+        {
+            pin = new();
+            password = new();
+            genPass = new int[4];
+        }
+
+        public void DecriptPassword(int option)
+        {
+
+        }
+
+        public void GeneratePassword()
+        {
+            Random random = new();
+            int digit = random.Next(0,9);
+            if (password.Contains(digit))
+            {
+                password.Add(digit);
+            }
+        }
+    }
+}
---
+++ EjerciciosHerramientasI/Models/Pin.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace EjerciciosHerramientasI.Models
+{
+    public partial class Pin
+    {
+        public int[] Password { get; set; }
+    }
+}
